f47e3112abe383f4d838c0900263e167f439e321
HyeonJooo <parkhj2416@gmail.com>
2024-05-22 11:06:09 +0900

    누락된 테스트 추가

--- src/test/java/com/example/demo/controller/UserControllerTest.java
+++ src/test/java/com/example/demo/controller/UserControllerTest.java
@@ -66,6 +66,13 @@ public class UserControllerTest {
 	}
 
 	@Test
+	void 사용자는_인증코드가_일치하지_않을_경우_권한없음_에러를_내려준다() throws Exception {
+		mockMvc.perform(get("/api/users/2/verify")
+				.queryParam("certificationCode", "aaaaaa-aaaa-aaa-aaaaaaaaaaac"))
+			.andExpect(status().isForbidden());
+	}
+
+	@Test
 	void 사용자는_내_정보를_불러올_때_개인정보인_주소도_갖고_올_수_있다() throws Exception {
 		mockMvc.perform(get("/api/users/me")
 				.header("EMAIL", "testId1"))
